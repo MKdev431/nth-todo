96e6e0da0d683ffc8126c6bfe12165bd4a2f8361
Michal Kurek (Mike) <michal@kurek.pl>
2023-10-04 18:57:38 +0200

    button fix

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -14,6 +14,7 @@ export default function Task({ id, title, completed, toggleTask, deleteTask }) {
         >
           delete
         </button>
+        <button>Edit task</button>
       </li>
     </>
   );
